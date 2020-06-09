5636f35fc629dbf1844258acbaf3d029bae3fd93
Jacob G. Wazydrag <jgw.jakegeorge@gmail.com>
2020-06-09 13:56:03 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -4,10 +4,9 @@ def find_item_by_name_in_collection(name, collection)
   # Consult README for inputs and outputs
 counter = 0 
 while counter < collection.length 
-  collection[counter]
+  
   counter += 1 
 end
-  
 end
 
 def consolidate_cart(cart)
