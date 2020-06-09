10a6db87f8611fb4da9af4fba02e4e7264182a08
Jacob G. Wazydrag <jgw.jakegeorge@gmail.com>
2020-06-09 14:12:28 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -15,13 +15,17 @@ def consolidate_cart(cart)
   while counter < cart.length
     new_cart_item = find_item_by_name_in_collection(cart[counter][:item], new_cart)
     if new_cart_item != nil
-      new_cart_item [:count] += 1 
+      new_cart_item [:count] += 1
+    else
+      new_cart_item = {
+        :item => name
+      }
     end
     counter += 1 
   end
 end
 
 
-# return [{:item => "AVACADO", :price => 3.00, :clearance => true, :counter => 3}]
-# item: {:item => "AVACADO", :price => 3.00, :clearance => true, :counter => 3}
+# return [{:item => "AVACADO", :price => 3.00, :clearance => true, :count => 3}]
+# item: {:item => "AVACADO", :price => 3.00, :clearance => true, :count => 3}
   
